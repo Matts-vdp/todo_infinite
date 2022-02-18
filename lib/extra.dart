988812d878a3a1531e7f74dc6b7cfe7ce4665c8e
Matts <vdpmatts@gmail.com>
2022-02-18 21:31:50 +0100

    add delayed notification support

--- lib/extra.dart
+++ lib/extra.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'Data/controller.dart';
 import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'notification_service.dart' if (Platform.isAndroid) "";
 import 'package:flutter/services.dart';
 
@@ -30,6 +31,7 @@ class Notifications extends StatefulWidget {
 class _NotificationsState extends State<Notifications> {
   final _formkey = GlobalKey<FormState>();
   final fieldText = TextEditingController();
+  DateTime time = DateTime.now();
   @override
   Widget build(BuildContext context) {
     final Controller c = Get.find();
@@ -57,21 +59,109 @@ class _NotificationsState extends State<Notifications> {
                   return null;
                 },
               ),
-              ElevatedButton(
-                onPressed: (){
-                  if (_formkey.currentState!.validate()) {
-                    if (Platform.isAndroid){ //notifications dont work on versions other then android
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Sending notification...')));
-                      c.incCnt();
-                      NotificationService().send(fieldText.text, "", c.getCnt());
-                    }
-                    else {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Notifications are not possible on this device.')));
-                    }
-                    fieldText.clear();
-                  }
-                }, 
-                child: Text("Send notification"),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Date: "+time.day.toString() + "/" + time.month.toString()),
+                    OutlinedButton(
+                        onPressed: () {
+                          showDatePicker(
+                            context: context, 
+                            initialDate: DateTime.now(), 
+                            firstDate: DateTime.now(), 
+                            lastDate: DateTime.now().add(Duration(days: 365)))
+                              .then((value) {
+                                if (value != null) {
+                                  setState(() {
+                                    time = DateTime(value.year, value.month, value.day, time.hour, time.minute);
+                                  });
+                                }
+                              });
+                        }, 
+                        child: Text("Pick date")
+                      ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Time: " + time.hour.toString() + ":" + time.minute.toString() + "h"),
+                    OutlinedButton(
+                      onPressed: () {
+                        showTimePicker(
+                          context: context, 
+                          initialTime: TimeOfDay.now(),
+                          builder: (BuildContext context, Widget? child) {
+                            return MediaQuery(
+                              data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+                              child: child!,
+                            );
+                          },
+                        ).then((value) {
+                          if (value != null) {
+                            setState(() {
+                              time = DateTime(time.year, time.month, time.day, value.hour, value.minute);
+                            });
+                          }
+                        });
+                      }, 
+                      child: Text("Pick time")
+                    ),
+                  ],
+                ),
+              ), 
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        FocusManager.instance.primaryFocus?.unfocus();
+                        if (_formkey.currentState!.validate()) {
+                          if (!kIsWeb && Platform.isAndroid){ //notifications dont work on versions other then android
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Sending notification...')));
+                            c.incCnt();
+                            NotificationService().send(fieldText.text, "", c.getCnt());
+                          }
+                          else {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Notifications are not possible on this device.')));
+                          }
+                          fieldText.clear();
+                        }
+                      }, 
+                      child: Text("Send now"),
+                    ),
+                    SizedBox(width: 10,),
+                    ElevatedButton(
+                      onPressed: () {
+                        FocusManager.instance.primaryFocus?.unfocus();
+                        if (_formkey.currentState!.validate()) {
+                          if (!kIsWeb && Platform.isAndroid){ //notifications dont work on versions other then android
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Sending notification...')));
+                            c.incCnt();
+                            if (time.isAfter(DateTime.now().add(Duration(seconds: 1)))) {
+                              NotificationService().sendDelay(fieldText.text, "", c.getCnt(), time);
+                            }
+                            else {
+                              NotificationService().send(fieldText.text, "", c.getCnt());
+                            }
+                          }
+                          else {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Notifications are not possible on this device.')));
+                          }
+                          fieldText.clear();
+                        }
+                      }, 
+                      child: Text("Send later"),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart'; 
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 // handles the sending of notifications
 class NotificationService {
@@ -13,6 +15,8 @@ class NotificationService {
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
   
   Future<void> init() async{
+    tz.initializeTimeZones();
+    tz.setLocalLocation(tz.getLocation("Europe/Brussels"));
     final AndroidInitializationSettings initializationSettingsAndroid =
       AndroidInitializationSettings('icon');
     final InitializationSettings initializationSettings =
@@ -73,9 +77,9 @@ class NotificationService {
     );
 
     NotificationDetails platformChannelSpecifics = 
-  NotificationDetails(android: androidPlatformChannelSpecifics);
+    NotificationDetails(android: androidPlatformChannelSpecifics);
 
-  await flutterLocalNotificationsPlugin.show(id, title, body, platformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(id, title, body, platformChannelSpecifics);
   }
 
   // used to send a notification with given title and body
@@ -83,7 +87,37 @@ class NotificationService {
     sendGroup().then((value) {
       sendnotif(title, body, id);
     });
-    
+  }
+  void sendDelay(String title, String body, int id, DateTime dtime) async{
+    const String groupKey = 'com.android.todo-infinite.GROUP';
+    const String groupChannelId = 'group';
+    const String groupChannelName = 'group';
+    AndroidNotificationDetails androidPlatformChannelSpecifics = 
+    AndroidNotificationDetails(
+        groupChannelId,   
+        groupChannelName, 
+        groupChannelName, 
+        importance: Importance.max,
+        priority: Priority.high,
+        visibility: NotificationVisibility.private,
+        playSound: true,
+        enableVibration: true,
+        groupKey: groupKey,
+        autoCancel: false,
+    );
+
+    NotificationDetails platformChannelSpecifics = 
+    NotificationDetails(android: androidPlatformChannelSpecifics);
+    tz.TZDateTime time = tz.TZDateTime.from(dtime, tz.local);
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      id, 
+      title, 
+      body, 
+      time, 
+      platformChannelSpecifics, 
+      uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime, 
+      androidAllowWhileIdle: true,
+    );
   }
    
 }
