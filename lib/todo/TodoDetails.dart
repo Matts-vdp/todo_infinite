f33579ace2282f98269e52b9e046e16939f7231b
Matts <vdpmatts@gmail.com>
2023-07-22 20:27:21 +0200

    add until picker

--- lib/todo/TodoDetails.dart
+++ lib/todo/TodoDetails.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:todo_infinite/data/todoData.dart';
 import '../data/controllers/TodoController.dart';
 
 class TodoDetails extends StatelessWidget {
@@ -20,13 +21,63 @@ class TodoDetails extends StatelessWidget {
             children: [
               SizedBox.fromSize(size: Size.fromHeight(10)),
               Text("Details", style: TextStyle(color: Colors.white30)),
-              IconButton(
-                  onPressed: (){todo.toggleFavorite(arr);},
-                  icon: Icon(todoData.favorite ? Icons.favorite : Icons.favorite_border, color: Colors.amber,)
-              ),
+              FavoriteButton(arr: arr, todoData: todoData),
+              UntilPicker(arr: arr, todoData: todoData),
             ],
           );
         }
+    );
+  }
+}
+
+class UntilPicker extends StatelessWidget {
+  const UntilPicker({
+    Key? key,
+    required this.arr,
+    required this.todoData,
+  }) : super(key: key);
+
+  final List<int> arr;
+  final TodoData todoData;
+
+  @override
+  Widget build(BuildContext context) {
+    final todo = Get.find<TodoController>();
+    return OutlinedButton(
+        onPressed: () {
+          showDatePicker(
+              context: context,
+              initialDate: DateTime.now(),
+              firstDate: DateTime.now(),
+              lastDate: DateTime.now().add(Duration(days: 365))
+          ).then((value) {
+            var time = value == null ? null : DateTime(value.year, value.month, value.day);
+            todo.setUntil(arr, time);
+          });
+        },
+        child: todoData.until != null ?
+          Text("Until: ${todoData.until?.day}/${todoData.until?.month}") :
+          Text("Until")
+    );
+  }
+}
+
+class FavoriteButton extends StatelessWidget {
+  const FavoriteButton({
+    Key? key,
+    required this.arr,
+    required this.todoData,
+  }) : super(key: key);
+
+  final List<int> arr;
+  final TodoData todoData;
+
+  @override
+  Widget build(BuildContext context) {
+    final todo = Get.find<TodoController>();
+    return IconButton(
+        onPressed: (){todo.toggleFavorite(arr);},
+        icon: Icon(todoData.favorite ? Icons.favorite : Icons.favorite_border, color: Colors.amber,)
     );
   }
 }
